287a3ec656134b17643ab679291088a8a22a291b
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-10-19 14:57:41 +0200

    Reduce randomness

--- ups/ups.c
+++ ups/ups.c
@@ -8,8 +8,15 @@ int main(int argc, char** argv) {
     }
     zsock_t * sc = zsock_new(ZMQ_PUB);
     zsock_connect(sc, "tcp://%s:5560", argv[1]);
+    bool state = 0;
+    int timeout = 0;
     while(!zsys_interrupted) {
-        if(((random())%2) >0) {
+        if(timeout == 0) {
+            state = !state;
+            timeout = 5 + random()%20;
+        }
+        timeout--;
+        if(state) {
             zstr_sendx(sc, argv[2], "ON", NULL);
             printf("%s ON\n", argv[2]);
         } else {
