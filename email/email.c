27ff14245108c75f0d20409625918e087258ec98
Michal Vyskocil <michalvyskocil@eaton.com>
2015-10-19 12:45:02 +0200

    Problem: email can't deal with new protocol
    Solution: adapt it

--- email/email.c
+++ email/email.c
@@ -1,14 +1,18 @@
 #include <czmq.h>
 #include <assert.h>
 
-int main() {
+int main(int argc, char** argv) {
+
     zsock_t *client = zsock_new_sub("tcp://*:5561", "");
     assert (client);
 
-    char *msg = zstr_recv(client);
+    char *msg, *ups, *state;
+	int r = zstr_recvx(client, &ups, &msg, &state, NULL);
     if (msg && streq(msg, "ALERT"))
-        zsys_info("Got ALERT, sending an email");
+        zsys_info("Got ALERT for ups '%s', state '%s', sending an email", ups, state);
     free(msg);
+    free(ups);
+    free(state);
 
     zsock_destroy(&client);
 }
