d07033bc582da3772617793cd8a97ba90e0b3e4f
Jim Klimov <jim@jimklimov.com>
2015-10-19 16:44:49 +0200

    zyre-trivial.c allow to parametrize node and chat names

--- zyre-trivial-src/zyre-trivial.c
+++ zyre-trivial-src/zyre-trivial.c
@@ -2,11 +2,13 @@
 
 int main(int argc, char**argv) {
         zsys_info("NOTE: If you do not see any peers, try\n  export ZSYS_INTERFACE=br1\n(whatever is correct for you) and re-run %s", argv[0]);
+        char *znodename = "jim";
+        char *zchatname = "BIOS";
 
-	zyre_t *node = zyre_new("jim");
+	zyre_t *node = zyre_new(znodename);
 	assert(node);
 	zyre_start(node);
-        zyre_join(node,"BIOS");
+        zyre_join(node,zchatname);
 
 	while (!zsys_interrupted) {
 		zyre_event_t *event = zyre_event_new (node);
