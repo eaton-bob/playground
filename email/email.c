3f5c72cab65ed02adc5be7958e571b71dd465f26
Michal Vyskocil <michalvyskocil@eaton.com>
2015-10-19 14:49:29 +0200

    Problem: there are more than one UPSes
    Solution: still receive alerts

--- email/email.c
+++ email/email.c
@@ -14,13 +14,16 @@ int main(int argc, char** argv) {
     zsock_t *client = zsock_new_sub(endpoint, "");
     assert (client);
 
-    char *msg, *ups, *state;
-	int r = zstr_recvx(client, &ups, &msg, &state, NULL);
-    if (msg && streq(msg, "ALERT"))
-        zsys_info("Got ALERT for ups '%s', state '%s', sending an email", ups, state);
-    free(msg);
-    free(ups);
-    free(state);
+    while (!zsys_interrupted) {
+        char *msg, *ups, *state;
+        int r = zstr_recvx(client, &ups, &msg, &state, NULL);
+        zsys_debug("ups = %s, msg = %s, state = %s\n", ups, msg, state);
+        if (msg && streq(msg, "ALERT"))
+            zsys_info("Got ALERT for ups '%s', state '%s', sending an email", ups, state);
+        zstr_free(&msg);
+        zstr_free(&ups);
+        zstr_free(&state);
+    }
 
     zsock_destroy(&client);
 }
