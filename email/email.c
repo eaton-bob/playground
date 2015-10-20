7fbb9c03a74c0e9143753e58a07f8d18c75ff4d1
Pieter Hintjens <ph@imatix.com>
2015-10-20 10:30:00 +0200

    Update email.c
    Code cleanups

--- email/email.c
+++ email/email.c
@@ -1,8 +1,4 @@
-#include <czmq.h>
 #include <zyre.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /* !\file    email.c
    \details  Listens on :5561 to receive alerts from counterparts like monitor
@@ -10,8 +6,8 @@
 */
 
 static char*
-s_find_the_endpoint(void) {
-
+s_find_the_endpoint(void)
+{
     zyre_t *node = zyre_new("email");
     assert (node);
 
@@ -27,65 +23,66 @@ s_find_the_endpoint(void) {
             break;
 
         if (zyre_event_headers (event)) {
-            const char* ip_recv = zyre_event_header (event, "GAP_SERVER");
-            if (ip_recv) {
-                endpoint = strdup(ip_recv);
-            }
+            const char *ip_recv = zyre_event_header (event, "GAP_SERVER");
+            if (ip_recv)
+                endpoint = strdup (ip_recv);
         }
-        //zstr_free(&ip_recv);
         zyre_event_print (event);
         zyre_event_destroy (&event);
     }
-
     zyre_stop (node);
     zyre_destroy (&node);
 }
 
-#define HB_WATREMARK 5
 
-int main(int argc, char** argv) {
+#define HB_WATERMARK 5
 
-    zsys_info("Getting the endpoint ...");
-    char *endpoint = s_find_the_endpoint();
+int main (int argc, char** argv) 
+{
+    zsys_info ("Getting the endpoint ...");
+    char *endpoint = s_find_the_endpoint ();
     assert (endpoint);
-    zsys_info("Got %s ...", endpoint);
-    zstr_free(&endpoint);
+    zsys_info ("Got %s ...", endpoint);
+    zstr_free (&endpoint);
 
-    zsock_t *client = zsock_new_sub(endpoint, "");
+    zsock_t *client = zsock_new_sub (endpoint, "");
     assert (client);
 
-    zpoller_t *pool = zpoller_new(client, NULL);
+    zpoller_t *pool = zpoller_new (client, NULL);
 
     int hb_missed = 0;
     while (!zsys_interrupted) {
         char *msg, *ups, *state;
-        zsocket_t *which = zpoller_wait(pool, 1000);
+        zsocket_t *which = zpoller_wait (pool, 1000);
 
-        // did not get the heartbeet at least
+        //  Did not get the heartbeet at least
         if (!which) {
             hb_missed += 1;
-            if (hb_missed < HB_WATREMARK)
+            if (hb_missed < HB_WATERMARK)
                 continue;
 
             hb_missed = 0;
-            zsys_info("Connection lost, getting the endpoint ...");
+            zsys_info ("Connection lost, getting the endpoint ...");
             char *endpoint = s_find_the_endpoint();
             assert (endpoint);
-            zsys_info("Got %s ...", endpoint);
-            zstr_free(&endpoint);
+            zsys_info ("Got %s ...", endpoint);
+            zstr_free (&endpoint);
         }
-
-        int r = zstr_recvx(which, &ups, &msg, &state, NULL);
-        if (msg && streq(msg, "ALERT"))
-            zsys_info("Got ALERT for ups '%s', state '%s', sending an email", ups, state);
-        else if (msg && streq(msg, "ART"))
-            zsys_debug("Got heartbeet message");
-        else
-            zsys_error("UNEXPECTED: ups = %s, msg = %s, state = %s\n", ups, msg, state);
-        zstr_free(&msg);
-        zstr_free(&ups);
-        zstr_free(&state);
+        int r = zstr_recvx (which, &ups, &msg, &state, NULL);
+        if (msg) {
+            if (streq (msg, "ALERT"))
+                zsys_info ("Got ALERT for ups '%s', state '%s', sending an email", 
+                    ups, state);
+            else 
+            if (streq(msg, "ART"))
+                zsys_debug("Got heartbeat message");
+            else
+                zsys_error ("UNEXPECTED: ups = %s, msg = %s, state = %s\n", 
+                ups, msg, state);
+        }
+        zstr_free (&msg);
+        zstr_free (&ups);
+        zstr_free (&state);
     }
-
-    zsock_destroy(&client);
+    zsock_destroy (&client);
 }
